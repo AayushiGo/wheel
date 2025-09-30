596e550128e32070d668d5b73649785d1e9eef8e
Aayushi Gorania <aayushigorania@gmail.com>
2025-09-30 11:45:18 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import "@styles/tailwind.css";
 
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import '@styles/tailwind.css';
 
-import App from "./App";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById("root")).render(
+import App from './App';
+
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>
